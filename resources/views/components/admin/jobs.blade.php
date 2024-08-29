c368d7d442f68c0c9b722602f54cac7146c5c618
John Lafitte <john.lafitte@gmail.com>
2024-08-29 00:30:52 -0500

    Change flow for admin view components

--- resources/views/components/admin/jobs.blade.php
+++ resources/views/components/admin/jobs.blade.php
@@ -4,17 +4,29 @@
     <tr>
       <th>Name</th>
       <th>Billing Code</th>
-      <th></th>
+      <th>Action</th>
     </tr>
   </thead>
   <tbody>
     @foreach (\App\Models\JobCode::all() as $job)
       <tr>
-        <td>{{ $job->name }}</td>
+        <td>
+          <a href="/admin/job?id={{ $job->id }}'">{{ $job->name }}</a>
+        </td>
         <td>{{ $job->billing_code }}</td>
-        <td><a href="/admin/job?job={{ $job->id }}'">Edit</a></td>
+        <td style="text-align: center">
+          <a href="/admin/job/delete?id={{ $job->id }}"
+            onclick="return confirm('Are you sure you want to delete this?')">Delete</a>
+        </td>
       </tr>
     @endforeach
   </tbody>
+  <tfoot>
+    <tr>
+      <td colspan="3">
+        <a href="/admin/job">Add New Job</a>
+      </td>
+    </tr>
+  </tfoot>
 </table>
 @endauth
--- resources/views/components/admin/users.blade.php
+++ resources/views/components/admin/users.blade.php
@@ -5,22 +5,32 @@
       <th>Name</th>
       <th>Email</th>
       <th>Admin</th>
-      <th></th>
+      <th>Actions</th>
     </tr>
   </thead>
   <tbody>
     @foreach (\App\Models\User::all() as $user)
       <tr>
-        <td>{{ $user->name }}</td>
+        <td>
+          <a href="/admin/user?id={{ $user->id}}">{{ $user->name }}</a>
+        </td>
         <td>{{ $user->email }}</td>
         <td>{{ $user->info->admin == 1 ? 'Yes' : 'No' }}</td>
-        <td>
-          <a href="/admin/user?user={{ $user->id}}">Edit</a>
-          |
+        <td style="text-align: center">
           <a href="/admin/entries?user={{ $user->id}}">Entries</a>
+          |
+          <a href="/admin/user/delete?id={{ $user->id }}"
+            onclick="return confirm('Are you sure you want to delete this user?')">Delete</a>
         </td>
       </tr>
     @endforeach
   </tbody>
+  <tfoot>
+    <tr>
+      <td colspan="4">
+        <a href="/admin/user">Add New User</a>
+      </td>
+    </tr>
+  </tfoot>
 </table>
 @endauth
